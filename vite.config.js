40477844fec165d5a7be125273a1005aea2909b0
Eddy <72110838+eddy64bcd@users.noreply.github.com>
2026-09-07 17:27:08 +0200

    Update base path in Vite configuration
    Set the base path for the project to '/projet10/'.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/projet10/', // remplace par le nom exact du repo, avec les slashs
 })
